a2da352e1dc594a02e056cef94445acae266d2ad
Benjamin Prud'homme <bgprudhomme@gmail.com>
2016-10-01 15:21:38 -0400

    Simplify MultiplicationTable

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,40 +1,33 @@
 public class MultiplicationTable {
 	public static void main(String[] args){
 		// First row (top heading)
-		System.out.print("X   ");
+		System.out.print("X    ");
 		for(int i=1; i<13; i++){
 			System.out.print(i);
-			if(i<10){
-				System.out.print("    ");
-			}
-			else{
-				System.out.print("   ");
-			}
+			applySpacing(i);
 		}
 		System.out.println();
 
 		// Rows 1-12
 		for(int currentRow = 1; currentRow<13; currentRow++){
 			System.out.print(currentRow);
-			if(currentRow<10){
-				System.out.print("   ");
-			}
-			else{
-				System.out.print("  ");
-			}
+			applySpacing(currentRow);
 			for(int multiplier = 1; multiplier<13; multiplier++){
-				System.out.print(currentRow*multiplier + " ");
-				if(currentRow*multiplier<10){
-					System.out.print("   ");
-				}
-				else if(currentRow*multiplier<100){
-					System.out.print("  ");
-				}
-				else{
-					System.out.print(" ");
-				}
+				System.out.print(currentRow*multiplier);
+				applySpacing(currentRow*multiplier);
 			}
 			System.out.println();
+		}
+	}
+	public static void applySpacing(int num){
+		if(num<10){
+			System.out.print("    ");
+		}
+		else if(num<100){
+			System.out.print("   ");
+		}
+		else{
+			System.out.print("  ");
 		}
 	}
 }
