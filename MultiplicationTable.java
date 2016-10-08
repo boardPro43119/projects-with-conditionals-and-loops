0cf031eb6de2c3954d7b74447cca843bd76a47c4
Benjamin Prud'homme <bgprudhomme@gmail.com>
2016-10-08 14:34:57 -0400

    Add comments describing steps in MultiplicationTable

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -7,6 +7,7 @@ public class MultiplicationTable {
 	// table as requested by the user.
 	//************************************************************************
 	public static void main(String[] args){
+		// Get range from user
 		Scanner in = new Scanner(System.in);
 		System.out.print("Start with row: ");
 		int startingRow = in.nextInt();
@@ -17,34 +18,40 @@ public class MultiplicationTable {
 		System.out.print("How many columns to print? ");
 		int columns = in.nextInt();
 
+		// Determine length of largest number in table
 		int columnLength = getColumnLength(startingRow, startingColumn, rows, columns);
 
 		// First row (top heading)
-		printNext("X", columnLength);
+		printNext("X", columnLength); // Start with multiplication symbol
 		for(int i=startingColumn; i<startingColumn+columns; i++){
+			// Print column numbers starting from startingColumns
 			printNext(Integer.toString(i), columnLength);
 		}
-		System.out.println();
+		System.out.println(); // Move to next row
 
-		// Rows 1-12
+		// All subesquent rows
 		for(int currentRow = startingRow; currentRow<startingRow+rows; currentRow++){
-			printNext(Integer.toString(currentRow), columnLength);
+			printNext(Integer.toString(currentRow), columnLength); // Print row number
 			for(int multiplier = startingColumn; multiplier<startingColumn+columns; multiplier++){
+				// On each row, print results of multiplying row number by column numbers
 				printNext(Integer.toString(currentRow*multiplier), columnLength);
 			}
-			System.out.println();
+			System.out.println(); // Move to next row
 		}
 	}
 
+	// Prints next number (or "X" at top left), adds appropriate spacing
 	public static void printNext(String str, int columnLength){
-		System.out.print(str);
+		System.out.print(str); // Print next number
 		for(int i=str.length(); i<= columnLength; i++){
+			// Add a space for each digit short of the maximum length, plus one
 			System.out.print(" ");
 		}
 	}
 
+	// Returns the number of digits in the largest number to be printed
 	public static int getColumnLength(int startingRow, int startingColumn, int rows, int columns){
-		int largestNumber = (startingRow+rows-1)*(startingColumn+columns-1);
-		return Integer.toString(largestNumber).length();
+		int largestNumber = (startingRow+rows-1)*(startingColumn+columns-1); // Calculate largest number to be printed
+		return Integer.toString(largestNumber).length(); // Returns number of digits in this number
 	}
 }
